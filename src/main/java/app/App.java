dae3a82b7e9ae4397df7f7abc24ed4e661cc6c21
LukaszKorzen <korzen1984@gmail.com>
2021-06-19 13:40:39 +0200

    UI

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,9 +1,16 @@
 package app;
 
+import org.apache.commons.cli.ParseException;
+
+import view.UI;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         System.out.println("Hello team");
+        
+     UI.parsearguments(args);  
+      
     }
 
 }
