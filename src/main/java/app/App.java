80a2a4bdb81f9cede2b1525c415c106d17d66a42
LukaszKorzen <korzen1984@gmail.com>
2021-06-19 13:51:44 +0200

    merge 1

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,5 +1,6 @@
 package app;
 
+<<<<<<< HEAD
 import org.apache.commons.cli.ParseException;
 
 import view.UI;
@@ -11,6 +12,21 @@ public class App {
         
      UI.parsearguments(args);  
       
+=======
+import org.apache.poi.ss.usermodel.Workbook;
+
+import services.ExcelReader;
+import services.PrintFirstReport;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        Workbook example1 = ExcelReader.openExcelFile();
+        
+        PrintFirstReport.printSheetNames(example1);
+        
+>>>>>>> refs/remotes/origin/master
     }
 
 }
