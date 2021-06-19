c3f7225ef0aae45999ac32adea1fed0ef2d29d7c
Marcin Kura <mkura@student.agh.edu.pl>
2021-06-19 15:51:50 +0200

    UI update

--- src/main/java/view/UI.java
+++ src/main/java/view/UI.java
@@ -8,27 +8,23 @@ import org.apache.commons.cli.ParseException;
 
 public class UI {
 
+    public static void parsearguments(String[] args) throws ParseException {
+//System.out.println("Witaj w aplikacji");
 
-	
-    
-    
-    public static void parsearguments(String[] args) throws ParseException{
-    	System.out.println("Witaj w aplikacji");
-    	
         Options options = new Options();
-    	
-    	options.addOption("t", false, "wyswietl raport pierwszy");
-    	
-    	
-    	CommandLineParser parser = new DefaultParser();
-    	CommandLine cmd = parser.parse( options, args);
-    	
-    	if(cmd.hasOption("t")) {
-    		System.out.println("udalo sie");
-    	}
-    	else {
-    		System.out.println("nieudalo sie");
-    	}
+
+        options.addOption("t", false, "wyswietl raport pierwszy");
+        options.addOption("r", false, "wyswietl raport pierwszy");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        if (cmd.hasOption("t")) {
+            System.out.println("raport1");
+        } else if (cmd.hasOption("r")) {
+            System.out.println("raport2");
+        } else {
+            System.out.println("pomoc");
+        }
     }
-    
 }
