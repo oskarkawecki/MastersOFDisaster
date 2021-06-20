867f32b89dc75b47a19d934181e01df0991ce7dc
Marcin Kura <mkura@student.agh.edu.pl>
2021-06-20 13:42:31 +0200

    FindFiles class added

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,11 +1,13 @@
 package app;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.commons.cli.ParseException;
 
 import model.Company;
 import services.ExcelReader;
+import services.FindFiles;
 import services.PrintFirstReport;
 
 public class App {
@@ -15,12 +17,18 @@ public class App {
 
     public static void main(String[] args) throws ParseException {
 
-        Company company = new Company();
-
-        company.addEmployee(ExcelReader.readEmployeeFromFile(new File(path)));
-        company.addEmployee(ExcelReader.readEmployeeFromFile(new File(path2)));
-
-        PrintFirstReport.printReport1(company, 2012);
+          ArrayList<String> filesPaths = FindFiles.getFiles("src/main/resources/reporter-dane/2012");
+          
+          for(String file : filesPaths) {
+              System.out.println(file);
+          }
+          
+//        Company company = new Company();
+//
+//        company.addEmployee(ExcelReader.readEmployeeFromFile(new File(path)));
+//        company.addEmployee(ExcelReader.readEmployeeFromFile(new File(path2)));
+//
+//        PrintFirstReport.printReport1(company, 2012);
 
 //        UI.parsearguments(args);
 
---
+++ src/main/java/services/FindFiles.java
@@ -0,0 +1,24 @@
+package services;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FindFiles {
+
+    public static ArrayList<String> filesPaths = new ArrayList<String>();
+
+    public static ArrayList<String> getFiles(String path) {
+
+        File folder = new File(path);
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                getFiles(file.getAbsolutePath());
+            } else {
+                if (file.getName().endsWith(".xls")) {
+                    filesPaths.add(file.getAbsolutePath());
+                }
+            }
+        }
+        return filesPaths;
+    }
+}
