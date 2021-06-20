2d8974f339bd48e762e05e3898cd8496536e16a0
LukaszKorzen <korzen1984@gmail.com>
2021-06-20 10:26:26 +0200

    help

--- src/main/java/view/UI.java
+++ src/main/java/view/UI.java
@@ -14,17 +14,29 @@ public class UI {
         Options options = new Options();
 
         options.addOption("t", false, "wyswietl raport pierwszy");
-        options.addOption("r", false, "wyswietl raport pierwszy");
+        options.addOption("r", false, "wyswietl raport drugi");
+        options.addOption("help", false, "wyswietl pomoc" );
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
 
         if (cmd.hasOption("t")) {
-            System.out.println("raport1");
+            System.out.println("Raport1");
+            
         } else if (cmd.hasOption("r")) {
-            System.out.println("raport2");
-        } else {
-            System.out.println("pomoc");
+        	
+            System.out.println("Raport2");
+            
+        } else if (cmd.hasOption("help")) {
+        	System.out.println("Opcje do wyboru:");
+        	System.out.println("Aby wybraæ raport pierwszy wpisz w konsole -t");
+            System.out.println("Aby wybraæ raport drugi wpisz w konsole -r");
+            System.out.println("Aby wybraæ zakres dat wpisz w konsole -from YYYY/MM/DD -to YYYY/MM/DD");
+            //System.out.println("Aby wybraæ raport za caly rok wpisz w  konsole YYYY");
+        } 
+        else {
+            System.out.println("Aby wybrac pomoc wpisz -help");
+           
         }
     }
 }
