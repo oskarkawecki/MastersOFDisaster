449bfdf92d18b6d9b6592d2f67938fa1fa2ea035
Dawid Stefanik <dstefanik@student.agh.edu.pl>
2021-06-21 19:19:39 +0200

    Exception improvements

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,9 +9,6 @@ import view.UI;
 public class App {
 
     public static void main(String[] args) throws ParseException {
-
-        String path = "src/main/resources/reporter-dane";
-        PrintSecondReport.printReport2(ExcelReader.company(path));
         UI.parsearguments(args);
     }
 
