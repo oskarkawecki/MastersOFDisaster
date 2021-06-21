15f9ede62f428b8483952ae42a58f9bd785d5cd8
Dawid Stefanik <dstefanik@student.agh.edu.pl>
2021-06-21 18:42:38 +0200

    Exceptions handle

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,12 +2,16 @@ package app;
 
 import org.apache.commons.cli.ParseException;
 
+import services.ExcelReader;
+import services.PrintSecondReport;
 import view.UI;
 
 public class App {
 
     public static void main(String[] args) throws ParseException {
 
+        String path = "src/main/resources/reporter-dane";
+        PrintSecondReport.printReport2(ExcelReader.company(path));
         UI.parsearguments(args);
     }
 
